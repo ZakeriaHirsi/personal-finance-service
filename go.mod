3e3ee48f727cdb6724dc31e41b5dd86c3152491e
Zakeria Hirsi <officialhirsi@gmail.com>
2024-07-13 13:10:36 +0100

    Adding hello world with open port and healthcheck

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/helloworld/go-app
+
+go 1.22.5
+
+require github.com/gofor-little/env v1.0.17
--- hello-world.go
+++ hello-world.go
@@ -1,7 +1,62 @@
 package main
-	
-import "fmt"
 
-func main(){
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gofor-little/env"
+)
+
+func main() {
 	fmt.Println("hello world")
+
+	var port = goDotEnvVariable("PORT")
+	if port == "" {
+		port = "8080" // Default port
+	}
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	http.HandleFunc("/", HelloHandler)
+	http.HandleFunc("/health", HealthHandler)
+
+	fmt.Println("Server listening on port", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	message := "Hello from GoLang API!"
+	encodeJSON(w, message)
+}
+
+func HealthHandler(w http.ResponseWriter, r *http.Request) {
+	message := "Health check success"
+	encodeJSON(w, message)
+}
+
+func encodeJSON(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		log.Println("Error encoding data:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
+
+// use godot package to load/read the .env file and
+// return the value of the key
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := env.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
 }
