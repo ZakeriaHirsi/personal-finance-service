b72f61711e39b6ec131bbb62f95a7c4b10eb6f5a
Zakeria Hirsi <officialhirsi@gmail.com>
2024-08-25 03:23:17 +0100

    Integrating into AWS cognito and listing available user pools

--- main.go
+++ main.go
@@ -1,100 +1,43 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
 	"log"
-	"net/http"
-	"os"
 
-	"example.com/app"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	cognito "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
-	"github.com/gofor-little/env"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider/types"
 )
 
 func main() {
-	log.SetOutput(os.Stdout)
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-
-	conf := &aws.Config{Region: aws.String("eu-west-1")}
-	sess, err := session.NewSession(conf)
+	sdkConfig, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
-		panic(err)
-	}
-
-	example := app.App{
-		CognitoClient:   cognito.New(sess),
-		UserPoolID:      goDotEnvVariable("COGNITO_USER_POOL_ID"),
-		AppClientID:     goDotEnvVariable("COGNITO_APP_CLIENT_ID"),
-		AppClientSecret: goDotEnvVariable("COGNITO_APP_CLIENT_SECRET"),
-	}
-	fmt.Println(goDotEnvVariable("COGNITO_USER_POOL_ID"))
-	fmt.Println(goDotEnvVariable("COGNITO_APP_CLIENT_ID"))
-	fmt.Println(goDotEnvVariable("COGNITO_APP_CLIENT_SECRET"))
-	//Endpoint handlers
-	http.HandleFunc("/health", HealthHandler)
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodPost:
-			// Handle html form submission
-			Call(&example, w, r)
-		}
-	})
-
-	var port = goDotEnvVariable("PORT")
-	if port == "" {
-		port = "8080" // Default port
-	}
-	fmt.Println("Server listening on port", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
-}
-
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	message := "Hello world!"
-	encodeJSON(w, message)
-}
-
-func HealthHandler(w http.ResponseWriter, r *http.Request) {
-	message := "Health check success from new ec2 instance"
-	encodeJSON(w, message)
-}
-
-// Call routes POST requests
-func Call(a *app.App, w http.ResponseWriter, r *http.Request) {
-
-	switch r.URL.Path {
-	case "/username":
-		a.Username(w, r)
-	default:
-		http.Error(w, fmt.Sprintf("Handler for POST %s not found", r.URL.Path), http.StatusNotFound)
-	}
-
-	return
-}
-
-func encodeJSON(w http.ResponseWriter, data interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(data)
-	if err != nil {
-		log.Println("Error encoding data:", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		fmt.Println("Couldn't load default configuration. Have you set up your AWS account?")
+		fmt.Println(err)
 		return
 	}
-}
+	cognitoClient := cognitoidentityprovider.NewFromConfig(sdkConfig)
+	fmt.Println("Let's list the user pools for your account.")
+	var pools []types.UserPoolDescriptionType
+	paginator := cognitoidentityprovider.NewListUserPoolsPaginator(
+		cognitoClient, &cognitoidentityprovider.ListUserPoolsInput{MaxResults: aws.Int32(10)})
 
-// use godot package to load/read the .env file and
-// return the value of the key
-func goDotEnvVariable(key string) string {
-
-	// load .env file
-	err := env.Load(".env")
-
-	if err != nil {
-		log.Fatalf("Error loading .env file")
+	for paginator.HasMorePages() {
+		output, err := paginator.NextPage(context.TODO())
+		if err != nil {
+			log.Printf("Couldn't get user pools. Here's why: %v\n", err)
+		} else {
+			pools = append(pools, output.UserPools...)
+		}
 	}
 
-	return os.Getenv(key)
+	if len(pools) == 0 {
+		fmt.Println("You don't have any user pools!")
+	} else {
+		for _, pool := range pools {
+			fmt.Printf("\t%v: %v\n", *pool.Name, *pool.Id)
+		}
+	}
 }
